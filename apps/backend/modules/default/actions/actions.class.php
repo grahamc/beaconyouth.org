2eaf1465848b632ffb7de113690f361370e4e996
Graham Christensen <graham.christensen@iamgraham.net>
2010-05-28 18:16:08 -0500

    Adding the backend module.

---
+++ apps/backend/modules/default/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * default actions.
+ *
+ * @package    Beacon
+ * @subpackage default
+ * @author     Graham Christensen
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class defaultActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+
+  }
+
+  public function executeModule(sfWebRequest $request) {
+      
+  }
+}
