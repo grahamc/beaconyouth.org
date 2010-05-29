c8b8d89bb413bc540ee7829316e4abb5065d2aaa
Graham Christensen <graham.christensen@iamgraham.net>
2010-05-29 18:12:48 -0500

    Making the error page look decent.

--- config/error/error.html.php
+++ config/error/error.html.php
@@ -1,1 +1,26 @@
-The developers here really buggered up. Sorry!
+CTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="title" content="Beacon Youth Conferences" />
+<meta name="description" content="Find all the conference forms for Beacon Youth evens in Florida." />
+<meta name="keywords" content="beacon youth, conference forms, beacon youth florida" />
+<meta name="language" content="en" />
+<meta name="robots" content="index, follow" />
+    <title>Beacon Youth Conferences</title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
+      </head>
+  <body>
+	  <div class="header">
+		  <h1>Beacon</h1>
+	  </div>
+	  <div class="container">
+		<h2>Yowzers!</h2>
+<p>Something really went wrong. We've got a commmittee checking it out, though.</p>
+	  </div>
+	  <div class="footer">
+		  <a href="/">Home</a> | <a href="/about/beacon">About Beacon</a> | <a href="/about/conferences">What's a Conference?</a> | <a href="/contact">Get in Touch</a> | <a href="/backend.php">Edit</a>	  </div>
+  </body>
+</html>
+
